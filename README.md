bd7d172564ff3742e86bbbb50c8ffa038cba6bba
Christopher Lam <whoischrislam@gmail.com>
2024-04-24 15:46:28 -1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Chris's Boilerplate for React + Vite
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Chris's React + Vite Boilerplate</h1>
+    </>
+  )
+}
+
+export default App
