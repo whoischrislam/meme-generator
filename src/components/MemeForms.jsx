1fbbde6dc77f9ecee4d73615c8ea89354cfa9258
whoischrislam <whoischrislam@gmail.com>
2024-04-25 12:42:06 -1000

    Update MemeForms.jsx

--- src/components/MemeForms.jsx
+++ src/components/MemeForms.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import data from "../memesData"
 
 export default function MemeForms() {
+    const [memeImage, setMemeImage] = useState("");
+    
     function handleSubmit (event) {
         event.preventDefault();
     }
@@ -8,21 +11,24 @@ export default function MemeForms() {
     function handleClick () {
         const randoNum = Math.floor(Math.random() * data.data.memes.length);
         const url = data.data.memes[randoNum].url;
-        console.log(url);
-        return url;
+        setMemeImage(url);
+        console.log(`memeImage set to: ${url}`);
     }
 
     return (
-        <form onSubmit={handleSubmit} id="getMeme" name="getMeme" method='GET' >
-            <div id='inputs'>
-                <label>Top Text
-                <input type='text' placeholder='Top text' />
-                </label>
-                <label>Bottom Text
-                <input id="bottom-text" type='text' placeholder='Bottom text' />
-                </label>
-            </div>
-            <button onClick={handleClick} id="memeButton">Get a new meme image 🌠</button>
-        </form>        
+        <>
+            <form onSubmit={handleSubmit} id="getMeme" name="getMeme" method='GET' >
+                <div id='inputs'>
+                    <label>Top Text
+                    <input type='text' placeholder='Top text' />
+                    </label>
+                    <label>Bottom Text
+                    <input id="bottom-text" type='text' placeholder='Bottom text' />
+                    </label>
+                </div>
+                <button onClick={handleClick} id="memeButton">Get a new meme image 🌠</button>
+            </form>
+            <img src={memeImage} />
+        </>
     )
 }
