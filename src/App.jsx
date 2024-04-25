bd9e09dbda124540caac0546d04e4fbb56af8d41
whoischrislam <whoischrislam@gmail.com>
2024-04-24 15:56:34 -1000

    Header compelte

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Header from "./components/Header"
 
 function App() {
   return (
     <>
-      <h1>Chris's React + Vite Boilerplate</h1>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Meme Generator</h1>
+            <h2>Make a meme!</h2>
+        </header>
+    );
+}
