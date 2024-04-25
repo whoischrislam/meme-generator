a37c326c5ba5af0b1d8bc3a229b17a61b7758aee
whoischrislam <whoischrislam@gmail.com>
2024-04-25 12:06:44 -1000

    added eventhandlers

--- src/components/MemeForms.jsx
+++ src/components/MemeForms.jsx
@@ -1,6 +1,19 @@
+import data from "../memesData"
+
 export default function MemeForms() {
+    function handleSubmit (event) {
+        event.preventDefault();
+    }
+    
+    function handleClick () {
+        const randoNum = Math.floor(Math.random() * data.data.memes.length);
+        const url = data.data.memes[randoNum].url;
+        console.log(url);
+        return url;
+    }
+
     return (
-        <form id="getMeme" name="getMeme" method='GET' >
+        <form onSubmit={handleSubmit} id="getMeme" name="getMeme" method='GET' >
             <div id='inputs'>
                 <label>Top Text
                 <input type='text' placeholder='Top text' />
@@ -9,7 +22,7 @@ export default function MemeForms() {
                 <input id="bottom-text" type='text' placeholder='Bottom text' />
                 </label>
             </div>
-            <button id="memeButton">Get a new meme image 🌠</button>
+            <button onClick={handleClick} id="memeButton">Get a new meme image 🌠</button>
         </form>        
     )
 }
