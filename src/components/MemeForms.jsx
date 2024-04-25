8f1e60affa5e1f4c2006e4f99ac6e50af0710ff5
whoischrislam <whoischrislam@gmail.com>
2024-04-24 16:28:06 -1000

    Accessibility improvement

--- src/components/MemeForms.jsx
+++ src/components/MemeForms.jsx
@@ -2,8 +2,12 @@ export default function MemeForms() {
     return (
         <form id="getMeme" name="getMeme" method='GET' >
             <div id='inputs'>
-            <input type='text' placeholder='Top text'></input>
-            <input type='text' placeholder='Bottom text'></input>
+                <label>Top Text
+                <input type='text' placeholder='Top text' />
+                </label>
+                <label>Bottom Text
+                <input id="bottom-text" type='text' placeholder='Bottom text' />
+                </label>
             </div>
             <button id="memeButton">Get a new meme image 🌠</button>
         </form>        
